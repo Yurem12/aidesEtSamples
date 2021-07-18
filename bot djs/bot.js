f655f4c0319a6a38f3b0489ebca0422ea1f1890a
Yurem12 <84666021+Yurem12@users.noreply.github.com>
2021-07-18 11:29:31 +0200

    1.0

--- bot djs/bot.js
+++ bot djs/bot.js
@@ -1,33 +1,79 @@
 const Discord = require('discord.js');
-const Client =  new Discord.Client;
-const prefix ="-";
-const prefix2 = "$";
+const config = require('./config.json');
+const fs = require('fs');
 
-Client.login('NjI5ODI0MTUzNzk2NjA4MDQw.XZfXcw.RyjkTG6k0iuB0-N0el3Fd7cxlQY')
+const bot = new Discord.Client();
+bot.commands = new Discord.Collection();
 
-Client.on('ready', () => {
-    console.log('LouKo0206');
+// Take commands
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+for (const file of commandFiles) {
+    const command = require(`./commands/` + file);
+    bot.commands.set(command.name, command);
+}
+
+// Cooldowns
+const cooldowns = new Discord.Collection();
+
+// On Ready
+bot.once('ready', () => {
+    console.log('Ready!');
 });
 
-const args = message.content.slice(prefix.length).trim();
+// On Message
+bot.on('message', message => {
+    if (!message.content.startsWith(config.prefix) || message.author.bot) return;
 
-Client.on('message', message => {
-    var args = message.content.slice(prefix.length).trim('');
-    var msgchannel = message.guild.channels.cache.get('841382474559127620');
-    if(message.author.bot) return;
-    if(message.channel.type == "dm") return;
+    const args = message.content.slice(config.prefix.length).split(/ +/);
+    const commandName = args.shift().toLowerCase();
 
-const member = `@${message.author.id}`;
+    const command = bot.commands.get(commandName) || bot.commands.find(cmd => cmd.aliases && cmd.aliases.includes(commandName));
 
+    // If command exist
+    if (!command) return;
 
-    if(message.channel.id === "841382474559127620"){
-        message.delete()
-        if (message.content.startsWith(`${prefix}SugSky`)){
-            const Suggestion = new Discord.MessageEmbed()
-                .setTitle("Suggestion De")
-                .setDescription(member)
-                .setColor("#6b0101")
-                .addField(`Suggestion : ${args}`)
-                msgchannel.send(Suggestion)
-    }}
+    // Check if command can be executed in DM
+    if (command.guildOnly && message.channel.type !== 'text') {
+        return message.reply('I can\'t execute that command inside DMs!');
+    }
+
+    // Check if args are required
+    if (command.args && !args.length) {
+        let reply = `You didn't provide any arguments, ${message.author}!`;
+
+        if (command.usage) {
+            reply += `\nThe proper usage would be: \`${config.prefix}${command.name} ${command.usage}\``;
+        }
+
+        return message.channel.send(reply);
+    }
+
+    // Check if user is in cooldown
+    if (!cooldowns.has(command.name)) {
+        cooldowns.set(command.name, new Discord.Collection());
+    }
+
+    const now = Date.now();
+    const timestamps = cooldowns.get(command.name);
+    const cooldownAmount = (command.cooldown || 3) * 1000;
+
+    if (timestamps.has(message.author.id)) {
+        const expirationTime = timestamps.get(message.author.id) + cooldownAmount;
+
+        if (now < expirationTime) {
+            // If user is in cooldown
+            const timeLeft = (expirationTime - now) / 1000;
+            return message.reply(`please wait ${timeLeft.toFixed(1)} more second(s) before reusing the \`${command.name}\` command.`);
+        }
+    } else {
+        timestamps.set(message.author.id, now);
+        setTimeout(() => timestamps.delete(message.author.id), cooldownAmount);
+        // Execute command
+        try {
+            command.execute(message, args);
+        } catch (error) {
+            console.error(error);
+            message.reply('there was an error trying to execute that command!');
+        }
+    }
 });
